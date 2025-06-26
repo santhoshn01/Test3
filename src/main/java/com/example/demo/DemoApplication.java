bced989e60268b9be456785cdcda78d5d47b2a7a
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-26 17:39:55 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,10 +7,14 @@ import org.springframework.boot.WebApplicationType;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication app = new SpringApplication(DemoApplication.class);
         app.setWebApplicationType(WebApplicationType.SERVLET); // ensure web server mode
         app.run(args);
         System.out.println("Welcome");
+        
+        // Pause for 10 seconds before shutting down the app
+        Thread.sleep(10000);
+        System.out.println("Exiting after 10 seconds");
     }
 }
