b2e09228083b1f5d00d4201793d6714e62a1f6b7
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-26 10:54:23 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,15 @@
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting DemoApplication...");
+        SpringApplication.run(DemoApplication.class, args);
+        logger.info("DemoApplication started successfully.");
+    }
 
 }
