bc2e2448430c244575235bbb9c039f3f3ded90e5
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-26 13:43:35 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,16 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(DemoApplication.class);
-        // Force the app to start as a web application with embedded servlet container
-        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.setWebApplicationType(WebApplicationType.SERVLET); // ensure web server mode
         app.run(args);
         System.out.println("Welcome");
     }
-
 }
