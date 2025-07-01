56aa4546d2074cccbb6f6c4b1367379e2c49cbd7
santhoshn01 <santhosh_n@chelsoft.com>
2025-07-01 16:41:22 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,18 +9,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(DemoApplication.class);
-        app.setWebApplicationType(WebApplicationType.SERVLET); // Starts a web server
+        app.setWebApplicationType(WebApplicationType.SERVLET);
         app.run(args);
 
         System.out.println("Welcome");
-
-        try {
-            Thread.sleep(600_000); // 10 minutes
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.out.println("Interrupted during sleep.");
-        }
-
-        System.out.println("Exiting after 10 minutes");
     }
 }
