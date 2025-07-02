70c4f07b84eee1f574838f47d4d1970bd68466c6
santhoshn01 <santhosh_n@chelsoft.com>
2025-07-02 15:34:42 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,14 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) throws InterruptedException {
-
+        SpringApplication.run(DemoApplication.class, args);
         if (!Boolean.getBoolean("skip.sleep")) {
             System.out.println("Sleeping for 10 minutes...");
             Thread.sleep(10 * 60 * 1000); // Sleep for 10 minutes
         } else {
             System.out.println("Skipping sleep during test runs.");
         }
-
-        SpringApplication.run(DemoApplication.class, args);
     }
 }
